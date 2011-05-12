bc6bf720e3caaf9ba5c779067211b3285e5ff208
Jaseg <s@jaseg.de>
2011-05-12 12:03:45 +0200

    Nothing exciting happened here, the initialisation routines are getting more complete, I did still not decide how to actually read the data and how to later transfer it to the computer.

--- ads1194.c
+++ ads1194.c
@@ -5,28 +5,31 @@
 #include "ads1194.h"
 
 /* FIXME/Note:
- * This method assumes nothing else than the ADS is on the spi in that the !CS pin of the ADS
- * is pulled low. If you want to change this, you will have to pull it low each time a command
- * or data is sent/to be received to/from the ADS.
+ * This method assumes nothing else than the ADS is on the spi when the !CS pin of the ADS is
+ * pulled low. If you want to change this, you will have to pull it low each time a command or
+ * data is sent/to be received to/from the ADS.
  */
 void init_ads_pass1(){
 	ADS_CKSEL_DDR |= 1<<ADS_CKSEL_PIN;
 	ADS_PWDN_DDR |= 1<<ADS_PWDN_PIN;
 	ADS_RESET_DDR |= 1<<ADS_RESET_PIN;
 	ADS_CS_DDR |= 1<<ADS_CS_PIN;
-	
-	ADS_PWDN_PORT |= 1<<ADS_PWDN_PIN;
-	ADS_RESET_PORT |= 1<<ADS_RESET_PIN;
+	ADS_START_DDR |= 1<<ADS_START_PIN;
+	SPI_DDR |= (1<<MOSI_PIN) | (1<<SCK_PIN);
+
+	//Select the ads's internal clock
 	ADS_CKSEL_PORT |= 1<<ADS_CKSEL_PIN;
+	//SPI enabled, master, \frac{f_{osc}}{64}=62500kHz data rate
+	SPCR = (1<<SPE) | (1<<MSTR) | (1<<SPR1);
 }
 
 /* FIXME/Note:
  * This method incorporates a 1ms delay. The delay needs to be at least 18 times the ocillator
- * period of the ads long, which is about 9ms in case of the internatl osc.
+ * period of the ads long, which is about 9ms in case of the internal osc.
  */
 void init_ads_pass2(){
 	ADS_RESET_PORT &= ~(1<<ADS_RESET_PIN);
-	nop(); nop(); nop(); nop(); nop(); nop(); nop(); nop(); //About 1us @ 1MHz. Not critical.
+	nop(); nop(); nop(); nop(); //About 1us @ 4MHz. Not critical.
 	ADS_RESET_PORT |= 1<<ADS_RESET_PIN;
 	_delay_ms(1); //That you'll have to cope with... or split this method, introduce a callback
 		      //etc.
--- ads1194.h
+++ ads1194.h
@@ -3,6 +3,19 @@
 #ifndef __ADS1194_H__
 #define __ADS1194_H__
 
-extern void init_ads(void);
+extern void ads_init_pass1(void);
+extern void ads_init_pass2(void);
+extern void ads_read_register(uint8_t address, uint8_t count, uint8_t* buffer);
+extern void ads_write_register(uint8_t address, uint8_t count, uint8_t* buffer);
+
+//ADS1X9X SPI commands
+#define ADS_WAKEUP	0x02
+#define ADS_STANDBY	0x04
+#define ADS_RESET	0x06
+#define ADS_START	0x08
+#define ADS_STOP	0x0A
+#define ADS_RDATAC	0x10
+#define ADS_SDATAC	0x11
+#define ADS_RDATA	0x12
 
 #endif//__ADS1194_H__
