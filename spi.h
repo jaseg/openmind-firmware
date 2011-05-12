6330feb2352b23619294b78e04ac30b078cb5a20
Jaseg <s@jaseg.de>
2011-05-12 23:12:25 +0200

    Added the license information (GPL-3)

--- spi.h
+++ spi.h
@@ -1,3 +1,20 @@
+/*   OpenMind open source eeg adc board firmware
+ *   Copyright (C) 2011  Sebastian Götte <s@jaseg.de>
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 //SPI interface driver
 
 #ifndef __SPI_H__
@@ -11,6 +28,6 @@
 #define SS_PIN		2
 
 extern void spi_init(void);
-extern uint8_t spi_send(uint8_t data);
+extern void spi_send(uint8_t data);
 
 #endif//__SPI_H__
